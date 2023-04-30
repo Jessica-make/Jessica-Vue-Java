3ae2fe2ad46dbe5b621ff962f782a07872c20062
赵江龙 <1164185650@qq.com>
2023-05-01 07:43:02 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import Cookies from 'js-cookie'
+
+import Element from 'element-ui'
+import './assets/styles/element-variables.scss'
+
+import './assets/icons' // icon
+
+import store from './store'
+import router from './router'
+
+Vue.config.productionTip = false
+
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
+
+new Vue({
+  store,
+  router,
+  render: h => h(App),
+}).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import app from './modules/app'
+import dict from './modules/dict'
+import user from './modules/user'
+import tagsView from './modules/tagsview'
+import permission from './modules/permission'
+import settings from './modules/settings'
+
+import getters from './getter'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    modules: {
+      app,
+      dict,
+      user,
+      tagsView,
+      permission,
+      settings
+    },
+    getters
+  })
+  
+  export default store
